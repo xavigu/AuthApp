555d3dcadf58b189425460ccb398ceabbb388d10
xavigu <xavigu91@gmail.com>
2019-10-27 13:37:08 +0100

    Add login/logout buttons funcionality and create component to handleAuthCallback

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PreciosComponent } from './components/precios/precios.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { CallbackComponent } from './components/callback/callback.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'precios', component: PreciosComponent},
   {path: 'protegida', component: ProtegidaComponent},
+  {path: 'callback', component: CallbackComponent},
   {path: '**', pathMatch:'full', component: HomeComponent },
 ];
 
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,6 +13,6 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit() {
+    console.log('Auth object', this.auth); 
   }
-
 }
